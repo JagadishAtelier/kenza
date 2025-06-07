803a319cab326d0bbc6f0ff8260c71f3f82eb33f
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-06-07 15:46:10 +0530

    worked on about us page

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -43,6 +43,25 @@ function AboutPage() {
             </div>
         ))}
       </div>
+
+
+      <div className='about-us-image-and-content'>
+        <div className='about-us-content'>
+            <h1>We Have Everything You Need ?</h1>
+            <p>Faded short sleeves t-shirt with high neckline. Soft and stretchy material for a comfortable fit. Accessorize with a straw hat and you're ready for summer!</p>
+            <div>
+                <h4>Sample Unordered List</h4>
+                <ul>
+                    <li>Lorem ipsum, or lipsum as it is sometimes known</li>
+                    <li>Dummy text used in laying out print, graphic or web designs</li>
+                    <li>The passage is attributed to.</li>
+                    <li>Proin molestie egestas orci ac suscipit risus posuere loremou.</li>
+                    <li>Dummy text used in laying out print, graphic or web designs</li>
+                </ul>
+            </div>
+        </div>
+        <img src={image2}/>
+      </div>
     </div>
   )
 }
