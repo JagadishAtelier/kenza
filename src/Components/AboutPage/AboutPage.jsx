7882299a6d050d02c45a81f2fc337cb4d5966921
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-06-07 16:26:03 +0530

    worked on about us page

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -5,6 +5,8 @@ import image2 from '../../Assets/aboutImage2.webp'
 import icon1 from '../../Assets/ab1.webp' 
 import icon2 from '../../Assets/ab2.webp' 
 import icon3 from '../../Assets/ab3.webp' 
+import LocationMap from '../LocationMap/LocationMap'
+import contactImage from '../../Assets/contactImage.webp'
 const iconContent = [
     {image : icon1 ,head : "FREE RESOURCES",para : "Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward"},
     {image : icon2 ,head : "MULTI-PURPOSE",para : "Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward"},
@@ -62,6 +64,56 @@ function AboutPage() {
         </div>
         <img src={image2}/>
       </div>
+
+
+      <div className='contact-us-container'>
+        <div className='contact-us-heading'>
+            <h1>Contact Us</h1>
+        </div>
+        <div className='contact-us-image-content'>
+            <div className='contact-us-image'>
+                <img src={contactImage}/>
+                <div className='contact-us-image-background'></div>
+            </div>
+            <div className='contact-us-right-side'>
+                <h1>GET IN TOUCH</h1>
+                <p>We'd Love to Hear From You, Lets Get In Touch!</p>
+                <div className='contact-address-grid'>
+                    <div className='contact-us-address'>
+                        <h5>Address</h5>
+                        <p>4005, Silver business point</p>
+                        <p>France</p>
+                    </div>
+                    <div className='contact-us-address'>
+                        <h5>Phone</h5>
+                        <p>+00 900 123456789</p>
+                    </div>
+                    <div className='contact-us-address'>
+                        <h5>Email</h5>
+                        <p>info@gmail.com</p>
+                    </div>
+                    <div className='contact-us-address'>
+                        <h5>Additional Information</h5>
+                        <p>We are open: Monday -</p>
+                        <p>Saturday, 10AM - 5PM and</p>
+                        <p>closed on sunday sorry for that.</p>
+                    </div>
+                </div>
+                <div className='conntact-us-social-icons'>
+                    <h5>Social</h5>
+                    <div className="social-icons">
+            <div className="icon facebook"><i className="bi bi-facebook"></i></div>
+            <div className="icon instagram"><i className="bi bi-instagram"></i></div>
+            <div className="icon tiktok"><i className="bi bi-tiktok"></i></div>
+            <div className="icon snapchat"><i className="bi bi-snapchat"></i></div>
+            <div className="icon vimeo"><i className="bi bi-vimeo"></i></div>
+          </div>
+                </div>
+            </div>
+        </div>
+
+      </div>
+      <LocationMap/>
     </div>
   )
 }
